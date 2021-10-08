1ff2be83666ea3eb968251283b3c815d1368b6e4
Rafael <rfernandes@soujmv.com>
2021-10-08 09:12:41 -0300

    mongo-init ajust

--- api/mongo-init/init.js
+++ api/mongo-init/init.js
@@ -13,14 +13,3 @@ db.createUser(
 		],
 	},
 );
-
-db.users.insert({
-	created_by: 'system',
-	updated_by: 'system',
-	name: 'Administrador',
-	user: 'admin',
-	// password = gd2D2@cjwcvneSMs2Sc_ew // Alterar para nova senha com encript novo
-	password: 'e025931cd226c575196f9a9f76de0125', // Alterar para nova senha com encript novo
-	is_deleted: false,
-	active: true,
-});
