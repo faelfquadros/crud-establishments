e93df7d58f46225a78e4741dfaeeae9b1ec758d2
rafaelfquadros <rafael.rffq@gmail.com>
2022-03-09 20:53:55 -0300

    feat: add general adjusts

--- front-end/src/components/header/header.component.js
+++ front-end/src/components/header/header.component.js
@@ -15,7 +15,7 @@ class Header extends React.Component{
     render(){
         return (
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                <a className="navbar-brand" href="#" onClick={this.props.onTitleClicked}>
+                <a className="navbar-brand" href="/#" onClick={this.props.onTitleClicked}>
                     {this.props.title}
                 </a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
